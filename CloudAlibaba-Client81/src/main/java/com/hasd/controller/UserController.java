ead9243f253a3a4a562300abc461f4d0cfe90464
hasdsd <1259305706@qq.com>
2023-02-14 21:02:28 +0800

    注册完成

--- CloudAlibaba-Client81/src/main/java/com/hasd/controller/UserController.java
+++ CloudAlibaba-Client81/src/main/java/com/hasd/controller/UserController.java
@@ -1,12 +1,16 @@
 package com.hasd.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hasd.config.NormalException;
+import com.hasd.entity.Code;
+import com.hasd.entity.Result;
 import com.hasd.entity.User;
 import com.hasd.mapper.UserMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 import java.util.List;
 
 /**
@@ -27,5 +31,20 @@ public class UserController {
         User user = users.get(2);
         System.out.println(user);
         return "success";
+    }
+
+    @PostMapping("/reg")
+    public Result register(@RequestBody User user, HttpServletRequest request) {
+        User userExist = userMapper.selectOne(new QueryWrapper<User>().eq("username", user.getUsername()));
+        if (userExist == null) {
+            throw new NormalException(Code.CODE_SERVER_ERROR, "用户名已存在");
+        }
+        user.setCreateTime(LocalDateTime.now());
+        user.setRegIp(request.getRemoteAddr());
+        int i = userMapper.insert(user);
+        if (i != 1) {
+            throw new NormalException(Code.CODE_SERVER_ERROR, "注册失败");
+        }
+        return Result.success("注册成功");
     }
 }

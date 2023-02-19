e62a7fcb52900121673bca0804538bc17d2a4e94
hasdsd <1259305706@qq.com>
2023-02-19 19:35:02 +0800

    gateway版本问题解决不了

---
+++ CloudAlibaba-GateWay7001/src/main/java/com/hasd/GateWay7001.java
@@ -0,0 +1,16 @@
+package com.hasd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : hasd
+ * @version 1.0.0
+ * @since : 2023/2/19 16:57
+ **/
+@SpringBootApplication
+public class GateWay7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWay7001.class, args);
+    }
+}

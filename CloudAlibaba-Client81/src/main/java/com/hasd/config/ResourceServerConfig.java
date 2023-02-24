9c81f0feb4e4054d23d2ca3a1b7b7ae5d877cad7
hasdsd <1259305706@qq.com>
2023-02-24 10:18:40 +0800

    我试最后一次

--- CloudAlibaba-Client81/src/main/java/com/hasd/config/ResourceServerConfig.java
+++ CloudAlibaba-Client81/src/main/java/com/hasd/config/ResourceServerConfig.java
@@ -1,11 +1,17 @@
 package com.hasd.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+
+import javax.annotation.Resource;
 
 /**
  * @author : hasd
@@ -16,9 +22,30 @@ import org.springframework.security.oauth2.config.annotation.web.configurers.Res
 @EnableResourceServer
 @Configuration
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+
+    @Bean
+    public ResourceServerTokenServices resourceServerTokenServices() {
+        RemoteTokenServices services = new RemoteTokenServices();
+        services.setClientId("cloud-client81");
+        services.setCheckTokenEndpointUrl("http://127.0.0.1:2001/oauth/check_token");
+        services.setClientSecret("secret");
+        return services;
+    }
+
+    @Resource
+    ResourceServerTokenServices resourceServerTokenServices;
+
+    @Resource
+    TokenStore tokenStore;
+
+    
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.resourceId("cloud-client81").stateless(true);
+        resources.resourceId("res1").stateless(true)
+                .tokenServices(resourceServerTokenServices)
+                .tokenStore(tokenStore)
+                .stateless(true);
     }
 
     @Override
---
+++ CloudAlibaba-Client81/src/main/java/com/hasd/config/TokenConfig.java
@@ -0,0 +1,31 @@
+package com.hasd.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+/**
+ * @author : hasd
+ * @version 1.0.0
+ * @since : 2023/2/24 09:30
+ **/
+
+@Configuration
+public class TokenConfig {
+    //加密使用的秘钥
+    public static final String SIGNING_KEY = "123";
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(SIGNING_KEY);
+        return converter;
+    }
+}
